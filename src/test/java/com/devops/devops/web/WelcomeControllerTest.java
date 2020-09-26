12f76c1badad98ccad337fce2bb048141c1b7112
Gabriel Crispim <gabrielcrispim@gmail.com>
2020-09-26 16:17:30 -0300

    fix

--- src/test/java/com/devops/devops/web/WelcomeControllerTest.java
+++ src/test/java/com/devops/devops/web/WelcomeControllerTest.java
@@ -6,16 +6,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @WebAppConfiguration
 class WelcomeControllerTest {
